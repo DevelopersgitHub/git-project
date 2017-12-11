45b8825ebfc6083012d1bdc3233ea3b28ae6d2e9
mihail <mikhailov.miail@yandex.ru>
2017-12-11 15:12:08 +0300

    user2

---
+++ less/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello,World!";
+echo "Hello,World_Test2!";
+echo "Hello,World_Test2!";
+echo "Hello,World_Test2!";
+echo "Hello,World_Test2!";
+
+echo "Hello,World_Test2!";
+echo "Hello,World_Test2!";
+echo "branch master";
+echo "branch master";
+
+echo "branch master";
+echo "branch master";echo "branch master";
+echo "branch master";echo "branch master";
+echo "branch master";echo "branch master";
+echo "branch master";
+echo "branch master";echo "branch master";echo "branch master";echo "branch master";
+
+?>
